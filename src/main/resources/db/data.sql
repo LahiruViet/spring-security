458dc85eb8bbd25da029d4526f7b29f5e66590b6
LahiruViet <48759351+LahiruViet@users.noreply.github.com>
2021-07-11 14:08:48 +0700

    Add files via upload

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO dev.T_USER (username, password, role)
+VALUES ('lahiru', '$2y$12$i02uba1IF6zfB70yaa/R8ui3ejpyXnW0YCnalJLZNne5qY5.giPG2', 'ADMIN');
+
+INSERT INTO dev.T_USER (username, password, role)
+VALUES ('immense', '$2y$12$FIHNldlyjdln0gLl1qHwcumRR7Nvfb2k0T/twC6vxcu6XtBWk4SUm', 'USER');
